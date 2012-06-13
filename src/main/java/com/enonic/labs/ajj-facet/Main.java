75713699ade6bbb2ce568d251873b9f0638fc679
Alex Rodriguez <aro@enonic.com>
2012-06-13 10:43:17 +0200

    Added pom file for intellij project.

--- src/main/java/com/enonic/labs/ajj-facet/Main.java
+++
@@ -1,7 +0,0 @@
-package com.enonic.labs.ajj-facet;
-
-public class Main {
-    public static final main (String args[]) {
-        System.out.println("We are the best!");
-    }
-}
---
+++ src/main/java/com/enonic/labs/ajjfacet/Main.java
@@ -0,0 +1,7 @@
+package com.enonic.labs.ajjfacet;
+
+public class Main {
+    public static final void main (String args[]) {
+        System.out.println("We are the best!");
+    }
+}
