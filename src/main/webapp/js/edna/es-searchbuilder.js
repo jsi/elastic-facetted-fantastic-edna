09bc10735d840d46e3dd8a33931f928ee1c1c466
Jørgen Sivesind <jsi@enonic.com>
2012-06-20 15:20:46 +0200

    Added some fields and methods to the searchBuilder.js, to build queries in this form: {     "query" : {         "filtered" : {         	"query" : {         		"bool" : {         			"must" : [         				{         				    "term" : {"resource" : "jsi"}         				},         				{                             "term" : {"billable" : true }                         }         			]}},             "filter" : {                 "numeric_range" : { 		        	"logDate" : { 					    "lt" : "2011/01/01",                         "gte" : "2010/01/01"                     }         		}         	}         }     },     "facets" : {         "tags" : {            "terms" :              {                 "field" : "resource"             }         }     } }

--- src/main/webapp/js/edna/es-searchbuilder.js
+++ src/main/webapp/js/edna/es-searchbuilder.js
@@ -5,11 +5,64 @@ Edna.SearchBuilder = function()
     var query = null;
     var filter = null;
     var facets = new Array();
+    var queryFacets = new Array();
+    var fromDate = null;
+    var toDate = null;
 
     this.setQuery = function( in_query )
     {
         query = in_query;
+        queryFacets = new Array();
+        fromDate = null;
+        toDate = null;
     };
+
+    this.setToDate = function ( in_toDate )
+    {
+        toDate = in_toDate;
+        query = null;
+    }
+
+    this.setFromDate = function ( in_fromDate )
+    {
+        fromDate = in_fromDate;
+        query = null;
+    }
+
+    this.addQueryFacet = function ( in_fieldName, in_value )
+    {
+        queryFacets.push ( '"term" : { "' + in_fieldName + '" : ' + in_value );
+        query = null;
+    }
+
+    this.getQuery = function ( )
+    {
+        if (query != null) {
+            return query;
+        } else {
+            var queryString = '{ "filtered" : {';
+            queryString += '"query" : {';
+            queryString += '"bool" : {';
+            queryString += '"must" : [';
+            for (var i = 0 ; i < queryFacets.length ; i++ )
+            {
+                queryString += '{';
+                queryString += queryFacets[i];
+                queryString += '}';
+                if (i < (queryString.length - 1)) {
+                    queryString += ",";
+                }
+            }
+            queryString += ']}},';
+            queryString += '"filter" : {';
+            queryString += '"numeric_range" : {';
+            queryString += '"logDate" : {';
+            queryString += '"lt" : "' + toDate + '",';
+            queryString += '"gte" : "' + fromDate + '"';
+            queryString += '}}}}}';
+            return queryString;
+        }
+    }
 
     this.setFilter = function( in_filter )
     {
